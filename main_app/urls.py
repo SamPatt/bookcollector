b7d6eaa06fcaaf723399984713798084fd5e5df3
SamPatt <git@sampatt.com>
2024-01-10 11:20:09 -0500

    Complete part 2

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name="about"),
-    path('books/', views.books_index, name="books" )
+    path('books/', views.books_index, name="books" ),
+    path('books/<int:book_id>/', views.books_detail, name='detail')
 ]
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
-books = [
-    {'title': '1984', 'author': 'George Orwell', 'genre': 'Dystopian', 'description': 'A novel about a totalitarian regime that uses surveillance and propaganda to control its citizens.', 'published_year': 1949},
-    {'title': 'To Kill a Mockingbird', 'author': 'Harper Lee', 'genre': 'Southern Gothic', 'description': 'A story of racial injustice and the destruction of innocence, set in the Deep South.', 'published_year': 1960},
-    {'title': 'Brave New World', 'author': 'Aldous Huxley', 'genre': 'Dystopian', 'description': 'A futuristic society driven by technological and genetic advancements, with disturbing undercurrents.', 'published_year': 1932},
-    {'title': 'The Great Gatsby', 'author': 'F. Scott Fitzgerald', 'genre': 'Tragedy', 'description': 'An exploration of the American Dream and its discontents in the Roaring Twenties.', 'published_year': 1925}
-]
+from .models import Book
+
+# books = [
+#     {'title': '1984', 'author': 'George Orwell', 'genre': 'Dystopian', 'description': 'A novel about a totalitarian regime that uses surveillance and propaganda to control its citizens.', 'published_year': 1949},
+#     {'title': 'To Kill a Mockingbird', 'author': 'Harper Lee', 'genre': 'Southern Gothic', 'description': 'A story of racial injustice and the destruction of innocence, set in the Deep South.', 'published_year': 1960},
+#     {'title': 'Brave New World', 'author': 'Aldous Huxley', 'genre': 'Dystopian', 'description': 'A futuristic society driven by technological and genetic advancements, with disturbing undercurrents.', 'published_year': 1932},
+#     {'title': 'The Great Gatsby', 'author': 'F. Scott Fitzgerald', 'genre': 'Tragedy', 'description': 'An exploration of the American Dream and its discontents in the Roaring Twenties.', 'published_year': 1925}
+# ]
 
 # Create your views here.
 def home(request):
@@ -15,6 +17,13 @@ def about(request):
     return render(request, 'about.html')
 
 def books_index(request):
+    books = Book.objects.all()
     return render(request, 'books/index.html', {
         'books': books
+    })
+
+def books_detail(request, book_id):
+    book = Book.objects.get(id=book_id)
+    return render(request, 'books/detail.html', { 
+        'book': book
     })
